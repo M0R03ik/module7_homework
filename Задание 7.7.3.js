a92c4a7f96a985740501bb956b9ca34c2f1af371
Mikhail Morozov <ya.moroz95@yandex.ru>
2024-03-15 14:12:56 +0300

    Выполнил все задания

---
+++ Задание 7.7.3.js
@@ -0,0 +1,10 @@
+// Задание 7.7.3
+// Есть массив объектов, представляющих собой пользователей, каждый из которых имеет свойство age (возраст). 
+// Создайте новый массив, содержащий только тех пользователей, чей возраст больше или равен 18. 
+// Затем создайте новый массив, содержащий только свойства name (имя) каждого пользователя.
+
+let users = [{name: "Bob", age: 50}, {name: "Julia", age: 10}, {name: "Dean", age: 33}, {name: "Sam", age: 30}, {name: "Jack", age: 17}, {name: "Derek", age: 18}]
+let adultUsers = users.filter(user => user.age >= 18)
+let namesUsers = adultUsers.map(user => user.name)
+console.log(adultUsers)
+console.log(namesUsers)
---
+++ Задание 7.7.4.js
@@ -0,0 +1,19 @@
+// Задание 7.7.4
+// Создайте функцию setFullName, которая будет устанавливать свойство fullName у переданного ей объекта. 
+// Используйте метод bind, чтобы создать новую функцию setPersonFullName, 
+// которая будет устанавливать fullName для объекта person из предыдущего примера. 
+// Вызовите setPersonFullName с параметром "John Smith" и убедитесь, 
+// что свойство fullName объекта person было изменено соответствующим образом.
+
+function setFullName(obj, fullName) {
+    obj.fullName = fullName
+  }
+  
+  const person = {
+    firstName: "John",
+    lastName: "Doe"
+  }
+  const setPersonFullName = setFullName.bind(null, person)
+  
+  setPersonFullName("John Smith")
+  console.log(person)
